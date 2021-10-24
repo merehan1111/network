12e65074054155c145cf997faead651508675a7e
merehan1111 <92991213+merehan1111@users.noreply.github.com>
2021-10-24 16:05:12 +0200

    Add files via upload

---
+++ week1/Account1.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package acount;
+
+class Account1 {
+     private int id = 0;
+    private double balance = 0.0;
+    private static double annualInterestRate = 0.0;
+    private java.util.Date dateCreated;
+
+    public Account1() {
+        dateCreated = new java.util.Date();
+    }
+
+    public Account1(int id, double balance) {
+        this();
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public String getDateCreated() {
+        return this.dateCreated.toString();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public double getMonthlyInterestRate() {
+        return (annualInterestRate / 100) / 12 ;
+    }
+
+    public double getMonthlyInterest() {
+        return balance * getMonthlyInterestRate();
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+    
+
+}
---
+++ week1/Acount.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package acount;
+
+/**
+ *
+ * @author NoteBook
+ */
+public class Acount {
+
+    
+    public static void main(String[] args) {
+        Account1 account = new Account1(1122, 20000);
+        account.setAnnualInterestRate(4.5);
+        account.withdraw(2500.0);
+        account.deposit(3000.0);
+        System.out.println("Balance: $" + account.getBalance());
+        System.out.println("Monthly Interest: " + account.getMonthlyInterest());
+        System.out.println("Date Created: " + account.getDateCreated());
+
+    }
+    
+}
